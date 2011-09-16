ea4e126153cabf6afff622c3a8df371ac33ac451
Titus Barik <titus@barik.net>
2011-09-16 17:00:10 -0400

    Added validators to Recipe.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ActiveRecord::Base
   belongs_to :category
+
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :instructions, :presence => true
+  validates :category, :presence => true
+
 end
